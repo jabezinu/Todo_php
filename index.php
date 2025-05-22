9efb5c1e5ee31b4ec119dc8f4c257a9482d85e80
jabezinu <jabezzinu31@gmail.com>
2025-05-22 23:43:15 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Todo list application using php and mysql</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="heading">
+        <h2>Todo list application using php and mysql</h2>
+    </div>
+</body>
+</html>
